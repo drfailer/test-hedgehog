4f4fb99768f9f6f58e5a4c16d2b35c5b8ccd5560
drfailer <drfailer.42@gmail.com>
2024-04-09 09:38:54 +0200

    fix: different result bug

--- matrix_vector_product_state/state/block_state.h
+++ matrix_vector_product_state/state/block_state.h
@@ -16,8 +16,8 @@ class BlockState: public hh::AbstractState<3,
       BlockState(size_t nbRows, size_t nbCols, size_t blockSize):
           nbRows_(nbRows), nbCols_(nbCols), blockSize_(blockSize),
           matrix_(nbRows * nbCols, nullptr),
-          vector_(nbRows, nullptr),
-          result_(nbCols, nullptr)
+          vector_(nbRows * nbCols, nullptr),
+          result_(nbRows * nbCols, nullptr)
         {}
 
       void execute(std::shared_ptr<MatrixBlockData<Type, Matrix>> matrixBlock) {
@@ -26,9 +26,9 @@ class BlockState: public hh::AbstractState<3,
           size_t row = matrixBlock->getBlockRow();
           size_t col = matrixBlock->getBlockCol();
 
-          if ((vectorBlock = vec(row)) && (resultBlock = res(col))) {
-              resetvec(row);
-              resetres(col);
+          if ((vectorBlock = vec(row, col)) && (resultBlock = res(row, col))) {
+              resetvec(row, col);
+              resetres(row, col);
               auto productBlock = std::make_shared<ProductBlockData<Type>>();
               productBlock->matrixBlockData = matrixBlock;
               productBlock->vectorBlockData = vectorBlock;
@@ -45,9 +45,9 @@ class BlockState: public hh::AbstractState<3,
           size_t row = vectorBlock->getBlockRow();
           size_t col = vectorBlock->getBlockCol();
 
-          if ((matrixBlock = mat(row, col)) && (resultBlock = res(col))) {
+          if ((matrixBlock = mat(row, col)) && (resultBlock = res(row, col))) {
               resetmat(row, col);
-              resetres(col);
+              resetres(row, col);
               auto productBlock = std::make_shared<ProductBlockData<Type>>();
               productBlock->matrixBlockData = matrixBlock;
               productBlock->vectorBlockData = vectorBlock;
@@ -64,9 +64,9 @@ class BlockState: public hh::AbstractState<3,
           size_t row = resultBlock->getBlockRow();
           size_t col = resultBlock->getBlockCol();
 
-          if ((matrixBlock = mat(row, col)) && (vectorBlock = vec(row))) {
+          if ((matrixBlock = mat(row, col)) && (vectorBlock = vec(row, col))) {
               resetmat(row, col);
-              resetvec(row);
+              resetvec(row, col);
               auto productBlock = std::make_shared<ProductBlockData<Type>>();
               productBlock->matrixBlockData = matrixBlock;
               productBlock->vectorBlockData = vectorBlock;
@@ -88,12 +88,16 @@ class BlockState: public hh::AbstractState<3,
         return matrix_[rIdx * nbRows_ + cIdx];
     }
 
-    std::shared_ptr<VectorBlockData<Type, Vector>> vec(size_t row) {
-        return vector_[row / blockSize_];
+    std::shared_ptr<VectorBlockData<Type, Vector>> vec(size_t row, size_t col) {
+        size_t rIdx = row / blockSize_;
+        size_t cIdx = col / blockSize_;
+        return vector_[rIdx * nbRows_ + cIdx];
     }
 
-    std::shared_ptr<VectorBlockData<Type, Result>> res(size_t col) {
-        return result_[col / blockSize_];
+    std::shared_ptr<VectorBlockData<Type, Result>> res(size_t row, size_t col) {
+        size_t rIdx = row / blockSize_;
+        size_t cIdx = col / blockSize_;
+        return result_[rIdx * nbRows_ + cIdx];
     }
 
     void mat(std::shared_ptr<MatrixBlockData<Type, Matrix>> ptr) {
@@ -103,11 +107,15 @@ class BlockState: public hh::AbstractState<3,
     }
 
     void vec(std::shared_ptr<VectorBlockData<Type, Vector>> ptr) {
-        vector_[ptr->getBlockRow() / blockSize_] = ptr;
+        size_t rIdx = ptr->getBlockRow() / blockSize_;
+        size_t cIdx = ptr->getBlockCol() / blockSize_;
+        vector_[rIdx * nbRows_ + cIdx] = ptr;
     }
 
     void res(std::shared_ptr<VectorBlockData<Type, Result>> ptr) {
-        result_[ptr->getBlockCol() / blockSize_] = ptr;
+        size_t rIdx = ptr->getBlockRow() / blockSize_;
+        size_t cIdx = ptr->getBlockCol() / blockSize_;
+        result_[rIdx * nbRows_ + cIdx] = ptr;
     }
 
     void resetmat(size_t row, size_t col) {
@@ -116,12 +124,16 @@ class BlockState: public hh::AbstractState<3,
         matrix_[rIdx * nbRows_ + cIdx] = nullptr;
     }
 
-    void resetvec(size_t row) {
-        vector_[row / blockSize_] = nullptr;
+    void resetvec(size_t row, size_t col) {
+        size_t rIdx = row / blockSize_;
+        size_t cIdx = col / blockSize_;
+        vector_[rIdx * nbRows_ + cIdx] = nullptr;
     }
 
-    void resetres(size_t col) {
-        result_[col / blockSize_] = nullptr;
+    void resetres(size_t row, size_t col) {
+        size_t rIdx = row / blockSize_;
+        size_t cIdx = col / blockSize_;
+        result_[rIdx * nbRows_ + cIdx] = nullptr;
     }
 
     size_t nbRows_ = 0;
