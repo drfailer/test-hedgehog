bd1e1689b05702dec07d9d7732e359c60f68b88f
drfailer <drfailer.42@gmail.com>
2024-04-17 12:51:48 +0200

    move some function in a utils file

--- gauss/gauss.cc
+++ gauss/gauss.cc
@@ -1,145 +1,50 @@
 #include <iostream>
 #include <memory>
 #include <utility>
-#include <random>
 
 #include "data/matrix.h"
 #include "data/vector.h"
 #include "graph/gauss_graph.h"
+#include "utils.h"
 
 using MatrixType = double;
 
-void generateRandomProblem(size_t nbVariables, MatrixType *matrix, MatrixType *vector, MatrixType *variables) {
-    std::random_device dv;
-    std::mt19937 gen(dv());
-    std::uniform_real_distribution<> dis(-100, 100);
-    size_t width = nbVariables, height = nbVariables;
-
-    for (size_t i = 0; i < nbVariables; ++i) {
-        variables[i] = dis(gen);
-    }
-
-    for (size_t i = 0; i < height; ++i) {
-        MatrixType sum = 0;
-        for (size_t j = 0; j < width; ++j) {
-            MatrixType value = dis(gen);
-            if (i == j && value == 0) value++; // make sure we don't have 0 on
-                                               // the diagonal (normally, we should
-                                               // exchange lines in this case
-                                               // but it's not done yet in the
-                                               // graph)
-            matrix[i * width + j] = value;
-            sum += value * variables[j];
-        }
-        vector[i] = sum;
-    }
-}
-
-bool isIdentity(Matrix<MatrixType> const& matrix, MatrixType precision) {
-    for (size_t i = 0; i < matrix.height(); ++i) {
-        for (size_t j = 0; j < matrix.width(); ++j) {
-            bool isOne = (1 - precision) <= matrix.get()[i * matrix.width() + j]
-                      && matrix.get()[i * matrix.width() + j] <= (1 + precision);
-            bool isZero = -precision <= matrix.get()[i * matrix.width() + j]
-                       && matrix.get()[i * matrix.width() + j] <= precision;
-            if ((i == j && !isOne) || (i != j && !isZero)) {
-                    std::cout << i << ", " << j << ": " << matrix.get()[i * matrix.width() + j] << std::endl;
-                    return false;
-            }
-        }
-    }
-    return true;
-}
-
-bool isTriangular(Matrix<MatrixType> const& matrix, MatrixType precision) {
-    for (size_t i = 0; i < matrix.height(); ++i) {
-        for (size_t j = 0; j <= i; ++j) {
-            bool isOne = (1 - precision) <= matrix.get()[i * matrix.width() + j]
-                      && matrix.get()[i * matrix.width() + j] <= (1 + precision);
-            bool isZero = -precision <= matrix.get()[i * matrix.width() + j]
-                       && matrix.get()[i * matrix.width() + j] <= precision;
-            if ((i == j && !isOne) || (i != j && !isZero)) {
-                    std::cout << i << ", " << j << ": " << matrix.get()[i * matrix.width() + j] << std::endl;
-                    return false;
-            }
-        }
-    }
-    return true;
-}
-
-bool verrifySolution(size_t nbVariables, MatrixType *founded, MatrixType *expected, MatrixType precision) {
-    bool output = true;
-
-    for (size_t i = 0; i < nbVariables; ++i) {
-        if (!((founded[i] - precision) <= expected[i] && expected[i] <= (founded[i] + precision))) {
-            output = false;
-            std::cout << "Error: " << expected[i] << " != " << founded[i] << std::endl;
-        }
-    }
-    return output;
-}
-
-/* test matrix for debugging */
-MatrixType* setupMatrix(size_t width, size_t height) {
-    MatrixType *matrixMem = new MatrixType[width * height]();
-
-    matrixMem[0] = 1;
-    matrixMem[1] = 2;
-    matrixMem[2] = -2;
-    matrixMem[3] = 2;
-    matrixMem[4] = 1;
-    matrixMem[5] = -5;
-    matrixMem[6] = 1;
-    matrixMem[7] = -4;
-    matrixMem[8] = 1;
-
-    // matrix for test
-    return matrixMem;
-}
-
-MatrixType* setupVector(size_t size) {
-    MatrixType *vectorMem = new MatrixType[size]();
-
-    vectorMem[0] = -15;
-    vectorMem[1] = -21;
-    vectorMem[2] = 18;
-
-    // vector for test
-    return vectorMem;
-}
-
 int main(int, char**) {
-    constexpr size_t matrixWidth = 1000;
-    constexpr size_t matrixHeight = 1000;
+    constexpr size_t matrixSize = 1000;
     constexpr size_t nbThreads = 4;
+    constexpr bool printMatrix = false;
+    constexpr bool printSolution = false;
 
-    MatrixType *matrixMem = new MatrixType[matrixWidth * matrixHeight]();
-    MatrixType *vectorMem = new MatrixType[matrixHeight]();
-    MatrixType *variablesMem = new MatrixType[matrixHeight]();
+    MatrixType *matrixMem = new MatrixType[matrixSize * matrixSize]();
+    MatrixType *vectorMem = new MatrixType[matrixSize]();
+    MatrixType *variablesMem = new MatrixType[matrixSize]();
 
-    generateRandomProblem(matrixHeight, matrixMem, vectorMem, variablesMem);
+    generateRandomProblem(matrixSize, matrixMem, vectorMem, variablesMem);
 
-    Matrix<MatrixType> matrix(matrixWidth, matrixHeight, matrixMem);
-    Vector<MatrixType> vector(matrixHeight, vectorMem);
+    Matrix<MatrixType> matrix(matrixSize, matrixSize, matrixMem);
+    Vector<MatrixType> vector(matrixSize, vectorMem);
     auto input = std::make_shared<std::pair<Matrix<MatrixType>, Vector<MatrixType>>>(matrix, vector);
-    /* std::cout << matrix << std::endl; */
-    /* std::cout << vector << std::endl; */
 
-    GaussGraph<MatrixType> gaussGraph(matrixHeight, nbThreads);
+    if (printMatrix) {
+        std::cout << matrix << std::endl;
+        std::cout << vector << std::endl;
+    }
+
+    GaussGraph<MatrixType> gaussGraph(matrixSize, nbThreads);
 
     gaussGraph.executeGraph();
     gaussGraph.pushData(input);
     gaussGraph.finishPushingData();
     gaussGraph.waitForTermination();
-
-    /* std::cout << matrix << std::endl; */
-    /* std::cout << vector << std::endl; */
-
-    gaussGraph.createDotFile("gausPivot.dot", hh::ColorScheme::EXECUTION, hh::StructureOptions::ALL);
+    gaussGraph.createDotFile("gausPivot.dot", hh::ColorScheme::EXECUTION, hh::StructureOptions::QUEUE);
 
     assert(isIdentity(matrix, 1e-3));
-    /* assert(isTriangular(matrix, 1e-3)); */
-    assert(verrifySolution(matrixHeight, vector.get(), variablesMem, 1e-3));
+    assert(isTriangular(matrix, 1e-3));
+    assert(verrifySolution(matrixSize, vector.get(), variablesMem, 1e-3));
+
+    if (printSolution) {
+        std::cout << vector << std::endl;
+    }
 
     delete[] matrixMem;
     delete[] vectorMem;
---
+++ gauss/utils.h
@@ -0,0 +1,83 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <cstddef>
+#include <random>
+#include <iostream>
+#include "data/matrix.h"
+
+template <typename Type>
+void generateRandomProblem(size_t nbVariables, Type *matrix, Type *vector, Type *variables) {
+    std::random_device dv;
+    std::mt19937 gen(dv());
+    std::uniform_real_distribution<> dis(-100, 100);
+    size_t width = nbVariables, height = nbVariables;
+
+    for (size_t i = 0; i < nbVariables; ++i) {
+        variables[i] = dis(gen);
+    }
+
+    for (size_t i = 0; i < height; ++i) {
+        Type sum = 0;
+        for (size_t j = 0; j < width; ++j) {
+            Type value = dis(gen);
+            if (i == j && value == 0) value++; // make sure we don't have 0 on
+                                               // the diagonal (normally, we should
+                                               // exchange lines in this case
+                                               // but it's not done yet in the
+                                               // graph)
+            matrix[i * width + j] = value;
+            sum += value * variables[j];
+        }
+        vector[i] = sum;
+    }
+}
+
+template <typename Type>
+bool isIdentity(Matrix<Type> const& matrix, Type precision) {
+    for (size_t i = 0; i < matrix.height(); ++i) {
+        for (size_t j = 0; j < matrix.width(); ++j) {
+            bool isOne = (1 - precision) <= matrix.get()[i * matrix.width() + j]
+                      && matrix.get()[i * matrix.width() + j] <= (1 + precision);
+            bool isZero = -precision <= matrix.get()[i * matrix.width() + j]
+                       && matrix.get()[i * matrix.width() + j] <= precision;
+            if ((i == j && !isOne) || (i != j && !isZero)) {
+                    std::cout << i << ", " << j << ": " << matrix.get()[i * matrix.width() + j] << std::endl;
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename Type>
+bool isTriangular(Matrix<Type> const& matrix, Type precision) {
+    for (size_t i = 0; i < matrix.height(); ++i) {
+        for (size_t j = 0; j <= i; ++j) {
+            bool isOne = (1 - precision) <= matrix.get()[i * matrix.width() + j]
+                      && matrix.get()[i * matrix.width() + j] <= (1 + precision);
+            bool isZero = -precision <= matrix.get()[i * matrix.width() + j]
+                       && matrix.get()[i * matrix.width() + j] <= precision;
+            if ((i == j && !isOne) || (i != j && !isZero)) {
+                    std::cout << i << ", " << j << ": " << matrix.get()[i * matrix.width() + j] << std::endl;
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename Type>
+bool verrifySolution(size_t nbVariables, Type *founded, Type *expected, Type precision) {
+    bool output = true;
+
+    for (size_t i = 0; i < nbVariables; ++i) {
+        if (!((founded[i] - precision) <= expected[i] && expected[i] <= (founded[i] + precision))) {
+            output = false;
+            std::cout << "Error: " << expected[i] << " != " << founded[i] << std::endl;
+        }
+    }
+    return output;
+}
+
+#endif
