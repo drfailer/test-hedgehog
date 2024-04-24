531d387e59fb7624962ddda8048799af67308199
drfailer <drfailer.42@gmail.com>
2024-04-24 12:53:38 +0200

    added a cuda lib that has a kernel for substracting matrix lines

---
+++ gauss_cuda/cuda/cuda_gauss.cu
@@ -0,0 +1,22 @@
+#include "cuda_gauss.h"
+
+__global__ void substractLines_impl(double *res, double *pivot, double *resVector,
+        double *pivotVector, double coef, size_t size) {
+    size_t i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < size) {
+        res[i] -= pivot[i] * coef;
+    }
+    if (i == 0) {
+        *resVector -= *pivotVector * coef;
+    }
+}
+
+void substractLines(double *res, double *pivot, double *resVector,
+        double *pivotVector, size_t coef, size_t size, cudaStream_t stream)
+{
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+    substractLines_impl<<<blocksPerGrid, threadsPerBlock, 0, stream>>>(
+            res, pivot, resVector, pivotVector, coef, size);
+}
